7626daaeb4ace85afe97d2d4c351bb65f89b348f
followmsi <followmsi@gmail.com>
2021-03-17 17:40:44 +0100

    dragon: Drop UserInfoWidget

--- dragon/device-vendor.mk
+++ dragon/device-vendor.mk
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Prebuilt APKs
-PRODUCT_PACKAGES += \
-    UserInfoWidget
-
 # Prebuilt privileged APKs
 PRODUCT_PACKAGES += \
     DragonKeyboardFirmwareUpdater \
